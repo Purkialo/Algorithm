5dc60d089cc694256abe2b1183e9bca6f5a6879a
Purkonw <wildpanda@yeah.net>
2018-01-28 21:06:35 +0800

    跳蛙问题2--斐波那契

---
+++ OFFER/9.cpp
@@ -0,0 +1,30 @@
+/**
+一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+求该青蛙跳上一个n级的台阶总共有多少种跳法。
+*/
+class Solution {
+public:
+    int jumpFloorII(int number) {
+		int target = 0;
+        if (number < 2)
+            return 1;
+        
+        for (int i = number - 1; i >= 0;i--)
+            target +=jumpFloorII(i);
+        return target;
+    }
+};
+/*
+class Solution {
+public:
+    int jumpFloorII(int number) {
+		if(number < 1){
+			return -1;
+		}
+		if(number == 1 || number == 2){
+			return number;
+		}
+		return 2 * jumpFloorII(number - 1);
+    }
+};
+*/
